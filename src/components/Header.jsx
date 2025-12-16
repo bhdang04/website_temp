8cbe1c0328b28a0d6885a6d7363453793c968b2f
Brian Dang <dangggbrian@gmail.com>
2025-12-15 18:48:13 -0800

    Made Mobile Menu Functional

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,15 @@
+import 'boxicons/css/boxicons.min.css';
 
 const Header = () => {
+    const toggleMobileMenu = () => {
+        const mobileMenu = document.getElementById('mobileMenu');
+        if(mobileMenu.classList.contains('hidden')){
+            mobileMenu.classList.remove('hidden');
+        } else {
+            mobileMenu.classList.add('hidden');
+        }
+    }
+
   return (
     <header className="flex justify-between items-center 
     py-4 px-4 lg:px-20"> 
@@ -24,9 +34,45 @@ const Header = () => {
             </a>
             <a className="text-base tracking-wider
             transition-colors hover-grey-300 z-50" href="#">
-                PRICING
+                DOCS
             </a>
         </nav>
+
+        <button className="hidden md:block 
+        bg-[#a7a7a7] text-black py-3 px-8 rounded-full
+        border-none font-medium transition-all duration-500
+        hover:bg-white cursor-pointer z-50">
+            SIGN IN
+        </button>
+
+        {/* Mobile Menu Button */}
+        <button onClick={toggleMobileMenu} 
+        className='md:hidden text-3xl p-2 z-50'>
+            <i class='bx bx-menu'></i> 
+        </button>
+
+        {/* Side Bar - Hidden */}
+        <div id='mobileMenu' className='fixed top-16 bottom-0 right-9 left-0 p5
+        md:hidden z-40 bg-black bg-opacity-70 backdrop_blur-md'>
+            <nav className='flex flex-col gap-6 items-center'>
+                 <a className="text-base tracking-wider
+                transition-colors hover-grey-300 z-50" href="#">
+                COMPANY
+            </a>
+            <a className="text-base tracking-wider
+                transition-colors hover-grey-300 z-50" href="#">
+                FEATURE
+            </a>
+            <a className="text-base tracking-wider
+                transition-colors hover-grey-300 z-50" href="#">
+                RESOURCES
+            </a>
+            <a className="text-base tracking-wider
+                transition-colors hover-grey-300 z-50" href="#">
+                DOCS
+            </a>
+            </nav>
+        </div>
     </header>
   )
 }
